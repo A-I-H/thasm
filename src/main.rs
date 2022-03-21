2c66abc3543478358286c86493fee480ee452ceb
MSB246 <msembulut@gmail.com>
2022-03-21 12:53:27 +0000

    Very basic language vm

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+pub mod vm;
+pub mod instructions;
+
+fn main() {
+
+}
---
+++ src/vm.rs
@@ -0,0 +1,72 @@
+use std::vec;
+use crate::instructions::*;
+
+pub struct VM {
+    registers: [i32; 32],
+    pc: usize,
+    program: Vec<u8>,
+}
+
+impl VM {
+    pub fn new() -> VM {
+        VM {
+            registers: [0; 32],
+            pc: 0,
+            program: vec![],
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            if self.pc >= self.program.len() {
+                break;
+            }
+
+            match self.decode_opcode() {
+                Opcode::HLT => {
+                    println!("HLT encountered");
+                    return;
+                },
+                _ => {
+                  println!("Unrecognized opcode found! Terminating!");
+                  return;
+                }
+            }
+        }
+    }
+
+    fn decode_opcode(&mut self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+        self.pc += 1;
+        return opcode;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_vm() {
+        let test_vm = VM::new();
+        assert_eq!(test_vm.registers[0], 0)
+    }
+
+    #[test]
+    fn test_opcode_hlt() {
+      let mut test_vm = VM::new();
+      let test_bytes = vec![0,0,0,0];
+      test_vm.program = test_bytes;
+      test_vm.run();
+      assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_opcode_igl() {
+      let mut test_vm = VM::new();
+      let test_bytes = vec![200,0,0,0];
+      test_vm.program = test_bytes;
+      test_vm.run();
+      assert_eq!(test_vm.pc, 1);
+    }
+}
